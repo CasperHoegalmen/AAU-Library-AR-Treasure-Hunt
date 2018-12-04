bc206076f2496787c5a40ba21f4f862358460fdb
MohanadZ <mzeito14@student.aau.dk>
2018-12-04 11:16:10 +0100

    AR Treasure Hunt Game_3D Arrows version

---
+++ Prototype/Assets/MiniGameChest.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGameChest : MonoBehaviour
+{
+    public bool isChestGameCompleted = false;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (isChestGameCompleted == false && 
+            GameObject.Find("ImageTargetChest").GetComponent<ChestDefaultTrackableEventHandler>().startMinigameChest == true)
+        {
+            GameObject.Find("Treasure").GetComponent<ChestAnimationController>().idleToShake = true;
+            GameObject.Find("Startscreen").GetComponent<textBubble>().pressCount = 0;
+            GameObject.Find("Startscreen").GetComponent<textBubble>().introContinued.current = true;
+            isChestGameCompleted = true;
+        }
+    }
+}
---
+++ Prototype/Assets/MiniGameEnding.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiniGameEnding : MonoBehaviour {
+
+    public bool isGameCompleted= false;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(GameObject.Find("ImageTargetChest").GetComponent<ChestDefaultTrackableEventHandler>().startMinigameEnding == true)
+        {
+            GameObject.Find("Treasure").GetComponent<ChestAnimationController>().idleToShake = true;
+            GameObject.Find("Treasure").GetComponent<ChestAnimationController>().shakeToOpen = true;
+            isGameCompleted = true;
+        }
+		
+	}
+}
